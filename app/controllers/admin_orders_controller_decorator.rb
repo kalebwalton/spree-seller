8d9c98335503050e441a1a1c8631c2589213c71b
Kaleb Walton <kalebwalton@gmail.com>
2012-01-11 05:17:14 +0000

    fixed admin order controller perms

--- app/controllers/admin_orders_controller_decorator.rb
+++ app/controllers/admin_orders_controller_decorator.rb
@@ -1,5 +1,6 @@
 Admin::OrdersController.class_eval do
   alias_method :index_admin, :index unless method_defined?(:index_admin)
+  alias_method :check_authorization_admin, :check_authorization unless method_defined?(:check_authorization_admin)
   
   def index
     # Scope orders down to the currently logged in seller
@@ -8,6 +9,16 @@ Admin::OrdersController.class_eval do
       params[:search][:seller_id_equals] = User.current.id
     end
     index_admin
+  end
+
+  # FIXME We had to remove the session[:access_token] from the authorize! method in order for this to work
+  def check_authorization
+    load_order
+    session[:access_token] ||= params[:token]
+
+    resource = @order || Order
+    action = params[:action].to_sym
+    authorize! action, resource
   end
 
 end
